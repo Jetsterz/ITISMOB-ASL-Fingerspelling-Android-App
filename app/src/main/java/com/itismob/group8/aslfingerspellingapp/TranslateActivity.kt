c6a140f659837e4c772cdb82ed4bbc841e6937bd
mayakayee <159097501+mayakayee@users.noreply.github.com>
2025-10-14 20:31:43 +0800

    camera implementation for practice/translate

--- app/src/main/java/com/itismob/group8/aslfingerspellingapp/TranslateActivity.kt
+++ app/src/main/java/com/itismob/group8/aslfingerspellingapp/TranslateActivity.kt
@@ -1,24 +1,39 @@
 package com.itismob.group8.aslfingerspellingapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.itismob.group8.aslfingerspellingapp.databinding.ActivityTranslateBinding
 
 class TranslateActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityTranslateBinding
+    private lateinit var camera: Camera
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityTranslateBinding.inflate(layoutInflater)
 
+        this.camera = Camera(this, viewBinding.pvCameraPreviewTranslate)
+        this.camera.setCaptureVideoButton(viewBinding.fabRecordTranslate)
+
+        if (camera.allPermissionsGranted()) {
+            camera.startCamera()
+        } else {
+            camera.requestPermissions()
+        }
+
         viewBinding.fabHomeTranslate.setOnClickListener {
             finish()
+            camera.closeCamera()
+        }
+
+        viewBinding.fabRecordTranslate.setOnClickListener {
+            camera.captureVideo()
+        }
+
+        viewBinding.fabSwitchCamTranslate.setOnClickListener {
+            camera.flipCamera()
         }
 
         setContentView(viewBinding.root)
-
     }
 }
