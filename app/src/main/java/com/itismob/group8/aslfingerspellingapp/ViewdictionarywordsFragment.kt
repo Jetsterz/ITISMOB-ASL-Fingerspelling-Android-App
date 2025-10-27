115fdebdfebe4a54bbb8f441ae67283bdd5401dc
mari-goround <maria_sophia_elauria@dlsu.edu.ph>
2025-10-28 07:30:29 +0800

    Finished the Dictionary Fragment.
    Some .idea files have also been auto-added/edited due to my shift in device (and subsequently, shift in Android Studio version).
    CreateWordActivity is unrelated, but created simultaneously.

---
+++ app/src/main/java/com/itismob/group8/aslfingerspellingapp/ViewdictionarywordsFragment.kt
@@ -0,0 +1,7 @@
+package com.itismob.group8.aslfingerspellingapp
+
+import androidx.fragment.app.Fragment
+import com.itismob.group8.aslfingerspellingapp.databinding.FragmentViewdictionarywordsBinding
+
+class ViewdictionarywordsFragment : Fragment(R.layout.fragment_viewdictionarywords) {
+}
