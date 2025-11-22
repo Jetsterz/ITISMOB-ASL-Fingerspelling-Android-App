fcc5517ed8231aa1e015a81cea93f73a22887ffd
mari-goround <maria_sophia_elauria@dlsu.edu.ph>
2025-11-22 23:48:01 +0800

    Added non-video editword too

--- app/src/main/java/com/itismob/group8/aslfingerspellingapp/wordlists/DisplayWordActivity.kt
+++ app/src/main/java/com/itismob/group8/aslfingerspellingapp/wordlists/DisplayWordActivity.kt
@@ -3,10 +3,13 @@ package com.itismob.group8.aslfingerspellingapp.wordlists
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.AutoCompleteTextView
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isGone
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.itismob.group8.aslfingerspellingapp.R
 import com.itismob.group8.aslfingerspellingapp.databinding.ActivityDisplayWordBinding
 import com.itismob.group8.aslfingerspellingapp.wordlists.database.DictioWordDatabase
 import com.itismob.group8.aslfingerspellingapp.wordlists.database.UserWordDatabase
@@ -58,9 +61,36 @@ class DisplayWordActivity : AppCompatActivity() {
             b.wordVideo.isGone = true
         }
         b.editCurrWord.setOnClickListener {
-            val newI = Intent(this, EditWordActivity::class.java)
-            newI.putExtra("id", id)
-            startActivity(newI)
+            val createDiaView = layoutInflater.inflate(R.layout.dialog_create_word, null)
+            createDiaView.findViewById<EditText>(R.id.nameIn).setHint(name)
+            createDiaView.findViewById<EditText>(R.id.defIn).setHint(def)
+            createDiaView.findViewById<AutoCompleteTextView>(R.id.catIn).setHint(cat)
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Edit Word")
+                .setView(createDiaView)
+                .setNegativeButton("Cancel") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .setPositiveButton("Proceed") { _, _ ->
+                    val newWord = createDiaView.findViewById<EditText>(R.id.nameIn).text.toString()
+                    val newDef = createDiaView.findViewById<EditText>(R.id.defIn).text.toString()
+                    val newCat = createDiaView.findViewById<AutoCompleteTextView>(R.id.catIn).text.toString()
+                    db.updateWord(Word(id, newWord, newDef, null, true, newCat))
+                    MaterialAlertDialogBuilder(this)
+                        .setTitle("Create Video?")
+                        .setMessage("Would you like to add an associated ASL video? The current video will be removed otherwise.")
+                        .setNegativeButton("Cancel") { vDialog, _ ->
+                            vDialog.dismiss()
+                            finish()
+                        }
+                        .setPositiveButton("Proceed to Video Creator") { _, _ ->
+                            val newI = Intent(this, EditWordActivity::class.java)
+                            newI.putExtra("id", id)
+                            startActivity(newI)
+                        }
+                        .show()
+                }
+                .show()
         }
         b.delCurrWord.setOnClickListener {
                 MaterialAlertDialogBuilder(this)
