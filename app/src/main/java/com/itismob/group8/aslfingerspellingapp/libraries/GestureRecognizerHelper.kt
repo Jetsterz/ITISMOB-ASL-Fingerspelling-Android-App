807aaf40bd4d7343db313b0c1faf0227ed3ebbda
mayakayee <159097501+mayakayee@users.noreply.github.com>
2025-12-01 02:35:30 +0800

    b

--- app/src/main/java/com/itismob/group8/aslfingerspellingapp/libraries/GestureRecognizerHelper.kt
+++ app/src/main/java/com/itismob/group8/aslfingerspellingapp/libraries/GestureRecognizerHelper.kt
@@ -174,9 +174,9 @@ class GestureRecognizerHelper (
 
         const val DELEGATE_CPU = 0
         const val DELEGATE_GPU = 1
-        const val DEFAULT_HAND_DETECTION_CONFIDENCE = 0.5F
-        const val DEFAULT_HAND_TRACKING_CONFIDENCE = 0.5F
-        const val DEFAULT_HAND_PRESENCE_CONFIDENCE = 0.5F
+        const val DEFAULT_HAND_DETECTION_CONFIDENCE = 0.7F
+        const val DEFAULT_HAND_TRACKING_CONFIDENCE = 0.7F
+        const val DEFAULT_HAND_PRESENCE_CONFIDENCE = 0.7F
         const val OTHER_ERROR = 0
         const val GPU_ERROR = 1
     }
